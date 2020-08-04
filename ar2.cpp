0e8e9e597ba567d6813a5cf83cfd68fb9e464010
Gon Cuabo <princesscuabo@Princesss-MacBook-Pro.local>
2020-08-04 08:52:27 +0800

    Get array and compute mean and median

---
+++ ar2.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  int n, current_input, index;
+  float mean, sum;
+
+  cout<<"Input n: "; 
+  cin>>n;
+
+  int array_n[n];
+
+  for (index = 0; index < n; index++) {
+    cout<<"Input number " << (index + 1) << ": "; 
+    cin >> current_input;
+    array_n[index] = current_input;
+
+    // prep for mean; get sum of inputs
+    sum += float(current_input);
+  }
+  cout<<"Your numbers are: "; 
+  for (index = 0; index < n; index++) {
+    cout << array_n[index] << ", ";
+  }
+
+  // get the mean
+  mean = sum / n;
+  cout << "\n\nMean is " << mean << endl;
+
+  // Sort array in ascending order
+  int i, j, temp;
+
+  for (i=0; i<n; i++) {
+    for(j=0; j<n; j++) {
+      if (array_n[i] > array_n[j]) {
+        temp = array_n[i];
+        array_n[i] = array_n[j];
+        array_n[j] = temp;
+      }
+    }
+  }
+
+  int middle_index; 
+  float median; 
+  // now that the array is sorted; we can get the median
+  // if n is odd, get the middle item
+  // if n is even, get the 2 middle items and get the average
+  if (n%2 == 1) {
+    middle_index = (n/2);
+    median = float(array_n[middle_index]);
+  } else {
+    middle_index = (n/2);
+    median = (float((array_n[middle_index])) + float(array_n[middle_index-1])) / 2;
+  }
+  cout << "Median is " << median << endl;
+
+  cout << "\n";
+  return 0;
+}
